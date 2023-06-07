7efe52a17f1ef0f52ec9db661d5ceaeb5843d718
akdeas oktanae <akdeaso@gmail.com>
2023-06-07 14:15:11 +0700

    feat: select jlpt level

--- src/JlptGenerator.jsx
+++ src/JlptGenerator.jsx
@@ -1,32 +1,100 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-function RandomJapaneseWord() {
+const RandomJapaneseWord = () => {
   const [word, setWord] = useState({});
+  const [level, setLevel] = useState("");
+  const [isLevelSelected, setIsLevelSelected] = useState(true);
 
   useEffect(() => {
+    if (level) {
+      axios
+        .get(
+          `https://jlpt-vocab-api.vercel.app/api/words/random?level=${level}`
+        )
+        .then((response) => {
+          setWord(response.data);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [level]);
+
+  const handleLevelChange = (event) => {
+    setLevel(event.target.value);
+    setIsLevelSelected(false);
+  };
+
+  const handleLevelSelect = () => {
+    setIsLevelSelected(true);
+    setWord({});
+  };
+
+  const handleNewWordClick = () => {
     axios
-      .get("https://jlpt-vocab-api.vercel.app/api/words/random")
-      .then((response) => setWord(response.data))
+      .get(`https://jlpt-vocab-api.vercel.app/api/words/random?level=${level}`)
+      .then((response) => {
+        setWord(response.data);
+      })
       .catch((error) => console.log(error));
-  }, []);
+  };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="text-center">
-        <div className="text-8xl font-bold mb-4">{word.word}</div>
-        <div className="text-3xl font-bold mb-2">{word.furigana}</div>
-        <div className="text-lg font-light mb-2">{word.meaning}</div>
-        <div className="text-lg font-light mb-8">Level: N{word.level}</div>
-        <button
-          onClick={() => window.location.reload(false)}
-          className="font-bold py-2 px-4 rounded"
-        >
-          Generate New Word
-        </button>
+    <div className="container mx-auto mt-8">
+      <div className="flex flex-col items-center">
+        {word.word ? (
+          <>
+            <div className="text-8xl font-bold">{word.word}</div>
+            <div className="text-3xl font-bold">{word.furigana}</div>
+            <div className="text-lg font-light">{word.meaning}</div>
+            <div className="text-lg font-light">Level: N{word.level}</div>
+            <div className="mt-4 flex flex-col items-center">
+              <button
+                onClick={handleLevelSelect}
+                className="font-bold py-2 px-4 rounded"
+              >
+                Choose Level
+              </button>
+              <button
+                onClick={handleNewWordClick}
+                className="font-bold py-2 mt-2 px-4 rounded"
+              >
+                Generate New Word
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className="mt-4 flex flex-col items-center">
+            {isLevelSelected ? (
+              <>
+                <label className="mb-2">Select JLPT Level:</label>
+                <select value={level} onChange={handleLevelChange}>
+                  <option value="">All Levels</option>
+                  <option value="1">N1</option>
+                  <option value="2">N2</option>
+                  <option value="3">N3</option>
+                  <option value="4">N4</option>
+                  <option value="5">N5</option>
+                </select>
+                <button
+                  onClick={handleNewWordClick}
+                  className="mt-4 font-bold py-2 px-4 rounded"
+                >
+                  Generate New Word
+                </button>
+              </>
+            ) : (
+              <button
+                onClick={handleLevelSelect}
+                className="font-bold py-2 px-4 rounded"
+              >
+                Choose Level
+              </button>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default RandomJapaneseWord;
